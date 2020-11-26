28b2246e1acf1e178372a2c6d0e5bea56ef1c35f
Jens Bannmann <jens.b@web.de>
2020-11-26 15:44:56 +0100

    Eliminate ByteBuddy dependency

--- src/main/java/com/github/bannmann/restflow/util/Types.java
+++ src/main/java/com/github/bannmann/restflow/util/Types.java
@@ -2,29 +2,75 @@ package com.github.bannmann.restflow.util;
 
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
+import lombok.NonNull;
+import lombok.ToString;
 import lombok.experimental.UtilityClass;
-
-import net.bytebuddy.ByteBuddy;
-import net.bytebuddy.description.modifier.Visibility;
-import net.bytebuddy.description.type.TypeDescription;
 
 @UtilityClass
 public class Types
 {
-    public <R> ParameterizedType listOf(Class<R> elementClass)
+    @ToString
+    private final static class ParameterizedTypeImpl implements ParameterizedType
     {
-        TypeDescription.Generic genericTypeDescription = TypeDescription.Generic.Builder.parameterizedType(List.class,
-            elementClass)
-            .build();
+        private final Type rawType;
+        private final Type[] actualTypeArguments;
 
-        Type fieldClass = new ByteBuddy().subclass(Object.class)
-            .defineField("dummyField", genericTypeDescription, Visibility.PUBLIC)
-            .make()
-            .load(Types.class.getClassLoader())
-            .getLoaded()
-            .getFields()[0].getGenericType();
-        return (ParameterizedType) fieldClass;
+        public ParameterizedTypeImpl(@NonNull Type rawType, @NonNull Type... arguments)
+        {
+            this.rawType = rawType;
+            this.actualTypeArguments = arguments.clone();
+        }
+
+        @Override
+        public Type[] getActualTypeArguments()
+        {
+            return actualTypeArguments.clone();
+        }
+
+        @Override
+        public Type getRawType()
+        {
+            return rawType;
+        }
+
+        @Override
+        public Type getOwnerType()
+        {
+            return null;
+        }
+
+        @Override
+        public boolean equals(Object o)
+        {
+            if (this == o)
+            {
+                return true;
+            }
+            if (o instanceof ParameterizedType)
+            {
+                ParameterizedType that = (ParameterizedType) o;
+                return Arrays.equals(actualTypeArguments, that.getActualTypeArguments()) &&
+                    rawType.equals(that.getRawType()) &&
+                    that.getOwnerType() == null;
+            }
+            return false;
+        }
+
+        @Override
+        public int hashCode()
+        {
+            int result = Objects.hash(rawType);
+            result = 31 * result + Arrays.hashCode(actualTypeArguments);
+            return result;
+        }
+    }
+
+    public ParameterizedType listOf(@NonNull Class<?> elementClass)
+    {
+        return new ParameterizedTypeImpl(List.class, elementClass);
     }
 }
